9db5301907b1ade6936956c1a32a441b78cc5cfa
Jason Storey <jasons@thwebco.com>
2018-07-20 12:43:00 -0700

    file watching stuff

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # filewatch
 Watch for files in directory and then do something
+
+# Install
+Download and Install NodeJS then run `npm install` this directory.
+
+### Dependencies
+`npm install -g pm2`
+
+### Running
+Open command prompt, `cd` into this directory and run...
+```shell
+pm2 start index.js
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require( 'path' );
+var fs = require( 'fs' );
+var request = require('request');
+var chokidar = require( 'chokidar' );
+
+var screenshot_dir = 'path/to/screenshots';
+
+chokidar.watch(screenshot_dir).on('change', (event, path) => {
+  var file = fs.readFileSync( path );
+
+  request.post( { url: 'http://api_url_here', body: file, }, ( err, res, body ) => {
+    console.log( body );
+  });
+});
